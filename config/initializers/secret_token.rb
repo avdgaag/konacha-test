c5c131014aa6f210420f303e988020d0c0f3d4a6
Arjan van der Gaag <arjan@Kabisa.nl>
2013-01-24 19:17:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KonachaTest::Application.config.secret_token = 'abbeeebd3bd8ee4e618125bd6dc9bf028b4cf340d7573025cc0c42161a3cec483684017e585f15a046728ef523d0220faacfaa5f412d6418676cf0c5b785b030'
---
+++ spec/javascripts/example_spec.js
@@ -0,0 +1,5 @@
+describe('Truth', function() {
+  it('is truthy', function() {
+    expect(true).to.be.ok;
+  });
+});
